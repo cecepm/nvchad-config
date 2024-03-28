f58f1b6d0c128eb868f40f225c7661afaa7dbbf1
Cecep Mahbub <cecep.mahbub@gmail.com>
2024-03-28 13:29:34 +0700

    Add mappings example

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- just for example, nvchad already have shortcut for formatting file, <Leader>fm
+--
+-- map("n", "<A-f>", function()
+--   vim.lsp.buf.format { async = true }
+-- end, { desc = "LSP Format"}
+-- )
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
