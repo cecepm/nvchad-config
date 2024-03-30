9926f93caf01999e2abdf96f5aadf1af32069ec3
Cecep Mahbub <cecep.mahbub@gmail.com>
2024-03-30 23:05:41 +0700

    Disable <F1>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- It can be really annoying when you keep hitting the F1 key accidentally,
+-- and the Help screen pops up. Here is a solution
+-- https://vim.fandom.com/wiki/Disable_F1_built-in_help_key
+map("n", "<F1>", "<nop>", { desc = "Press F1, but do nothing" })
+
+
 -- just for example, nvchad already have shortcut for formatting file, <Leader>fm
 --
 -- map("n", "<A-f>", function()
